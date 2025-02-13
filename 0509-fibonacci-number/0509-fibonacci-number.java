69448db09284eda5704aa52c3e52a92e3e6cdd67
Nikita Patil <119148400+Nikita-Patil5@users.noreply.github.com>
2025-02-14 00:41:32 +0530

    Time: 0 ms (100%), Space: 40.6 MB (26.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,7 +4,15 @@ class Solution {
         {
             return n;
         }
+        int first = 0, second = 1, third = 0;
+       for (int i = 2; i <= n; i++)
+        {
+           
+            third = first + second;
+            first = second;
+            second = third;
+        }
 
-        return fib(n - 1) + fib(n - 2);
+        return third;
     }
 }
