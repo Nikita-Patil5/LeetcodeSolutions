f687ebbee63cc8cbf544d282d192d8dd5a838e38
Nikita Patil <119148400+Nikita-Patil5@users.noreply.github.com>
2025-02-17 20:50:29 +0530

    Time: 2 ms (99.3%), Space: 46.2 MB (21.03%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,31 +1,18 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new LinkedList<>(); 
         int pointer = num.length - 1;
-        int carry = 0;
 
-        while(pointer >= 0 || k > 0){
-            int numval = 0;
-
-            if(pointer >= 0){
-                numval = num[pointer];
+        while (pointer >= 0 || k > 0) {
+            if (pointer >= 0) {
+                k += num[pointer]; 
+                pointer--;
             }
-
-            int lastDigit = k % 10;
-            int sum = numval + lastDigit + carry;
-
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ans.add(digit);
-            pointer--;
-            k = k / 10;
-        }
-        if(carry > 0){
-            ans.add(carry);
+            
+            ans.add(0, k % 10); 
+            k /= 10; 
         }
 
-        Collections.reverse(ans);
         return ans;
     }
 }
