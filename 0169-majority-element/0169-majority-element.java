6b9fd9665c4d7130991ec0fd5919451a7bfe6881
Nikita Patil <119148400+Nikita-Patil5@users.noreply.github.com>
2025-07-27 14:38:08 +0530

    Time: 2 ms (73.79%), Space: 53.6 MB (8.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int candidate = 0; // Can be initialized to any value
+         int candidate = 0; 
         int count = 0;
 
         for (int num : nums) {
-            // If count is 0, we choose a new candidate
             if (count == 0) {
                 candidate = num;
             }
 
-            // If the current number is our candidate, we increment the count.
-            // Otherwise, we decrement it.
             if (num == candidate) {
                 count++;
             } else {
                 count--;
             }
         }
-        
-        // Because the problem guarantees a majority element exists,
-        // the final candidate is our answer. No second pass is needed.
         return candidate;
-    }}
+    }
+}
