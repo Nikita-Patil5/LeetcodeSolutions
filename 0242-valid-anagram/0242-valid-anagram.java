3946166d64ec0a83fc697a31a801d7577c91e2d0
Nikita Patil <119148400+Nikita-Patil5@users.noreply.github.com>
2026-04-25 21:25:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,53 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+
+//         int [] counts = new int[26];
+
+//         for(int i = 0; i < s.length(); i++){
+//             counts[s.charAt(i) - 'a']++;
+//         }
+
+//         for(int i = 0; i < t.length(); i++){
+//             counts[t.charAt(i) - 'a']--;
+//         }
+
+//         for(int count : counts){
+//             if(count != 0){
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length()){
+        if (s.length() != t.length()) {
             return false;
         }
 
-        int [] counts = new int[26];
+        Map<Character, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < s.length(); i++){
-            counts[s.charAt(i) - 'a']++;
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
-        for(int i = 0; i < t.length(); i++){
-            counts[t.charAt(i) - 'a']--;
-        }
-
-        for(int count : counts){
-            if(count != 0){
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c)) {
                 return false;
+            }
+
+            map.put(c, map.get(c) - 1);
+
+            if (map.get(c) == 0) {
+                map.remove(c); 
             }
         }
 
-        return true;
+        return map.isEmpty();
     }
 }
-
